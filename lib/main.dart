89424133b7f490f97558f934924fd820eec197b2
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-12-28 01:27:36 +0200

    feat: initialize Firebase and set up routing for login, signup, and weather details screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'login_screen.dart';
+import 'signup_screen.dart';
+import 'weather_details_screen.dart';
 import 'weather_forecast_next.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(WeatherForecastNext());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MaterialApp(
+    initialRoute: '/signup',
+    routes: {
+      '/login': (context) => const LoginScreen(),
+      '/signup': (context) => const SignUpScreen(),
+      '/weather_details': (context) => const WeatherDetailsScreen(),
+      '/weather_forecast_next': (context) => WeatherForecastNext(),
+      // '/home': (context) => const HomeScreen(),
+    },
+  ));
 }
-
